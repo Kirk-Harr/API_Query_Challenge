671aac562ba453724e980fe7dcbde9c684e553ba
Kirk Harr <daehlie@gmail.com>
2017-03-07 17:37:28 -0800

    Implemented the StateIdLookup class to retrieve state FIPS ID by providing the state name as a string.

--- src/main/java/com/mindbeta/javachallenge/CensusDataController.java
+++ src/main/java/com/mindbeta/javachallenge/CensusDataController.java
@@ -1,5 +1,14 @@
 package com.mindbeta.javachallenge;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
 /**
  * Gathers Aggregate Demographic results from US Census Data by State.
  *
@@ -7,12 +16,13 @@ package com.mindbeta.javachallenge;
  */
 public class CensusDataController
 {
+    // Set static User-Agent for all connections.
+    private final String USER_AGENT = "Mozilla/5.0";
     /**
      * Gather demographic data for a list of US states from the US Census data for those states and displaying an aggregate output of that data. Format options include a weighted average for income distribution below poverty of all the states, and CSV output for all states individually.
      * @param args comma-delimited list of US States followed by 'CSV' for CSV output and 'averages' for the weighted-average income below poverty output.
      */
     public static void main( String[] args ) {
-
     }
 
     /**
@@ -21,8 +31,42 @@ public class CensusDataController
      * @param stateName the name of the state to lookup for it's US Census FIPS ID.
      * @return US Census FIPS ID for the state.
      */
-    public int StateIdLookup( String stateName ) {
-        return 0;
+    public Integer StateIdLookup( String stateName ) {
+        // URL of Census Data API with proper request options.
+        String url = "https://www.broadbandmap.gov/broadbandmap/census/state/" + stateName + "?maxresults=1&all=false&format=json";
+        StringBuffer response = new StringBuffer();
+        try {
+            URL apiRequest = new URL(url);
+            HttpsURLConnection connection = (HttpsURLConnection) apiRequest.openConnection();
+            connection.setRequestMethod("GET");
+            // Provide dummy User-Agent.
+            connection.setRequestProperty("User-Agent", USER_AGENT);
+            if (connection.getResponseCode() == 200){
+                BufferedReader input = new BufferedReader( new InputStreamReader(connection.getInputStream()));
+                String line;
+                // Gather JSON output line by line.
+                while ((line = input.readLine()) != null ){
+                    response.append(line);
+                }
+                input.close();
+            }
+            // In the event of a unsuccessful GET return error condition.
+            else {
+                System.out.println("Error retrieving URL from remote API. Please try again later.");
+            }
+        }
+        // Any exceptions caught during the operations will print out a stack trace for debugging.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        // Unboxing the JSON data structure.
+        JSONObject fullOutput = new JSONObject(response.toString());
+        JSONObject results = fullOutput.getJSONObject("Results");
+        JSONArray stateDataArray = results.getJSONArray("state");
+        JSONObject stateData = (JSONObject) stateDataArray.get(0);
+        // Return just the FIPS ID.
+        Integer returnInt = Integer.valueOf((String)stateData.get("fips"));
+        return returnInt;
     }
 
     /**
